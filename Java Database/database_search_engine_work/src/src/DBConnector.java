19e32a18ac6ba854dae09fa47cd0e4c5efe2ac9d
ClarkthyLord <clarck71@outlook.com>
2016-03-09 22:47:39 -0800

    Started to work on login cheking:P

--- Java Database/database_search_engine_work/src/src/DBConnector.java
+++ Java Database/database_search_engine_work/src/src/DBConnector.java
@@ -5,35 +5,43 @@ import java.sql.*;
 public class DBConnector {
 
 	//VARIABLES
-	static public String DBName, name, id, date_1, date_2, output;
+	static public String DBName, DBData,name, id, date_1, date_2, output;
 	static public String line = System.getProperty("line.separator");//Line Separator
 
 	static public boolean connection_check;
 	//DATABASE VARIABLES
 	static public Connection connection;
-	static public Statement statement;
+	static public PreparedStatement statement;
 	static public ResultSet DB_result;
 
 	public static void DBConnection(){
 	
-		System.out.println("Retrieving data fron database...");
+		System.out.println("Connecting to database...");
 	
 		try{
 		
 			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/school_data_base", "root", "");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/java_database", "root", "");
 		
-			System.out.println("Database Name: " + DBName + line + "Data:");
 			connection_check = true;
+			System.out.println("Retrieving data from database.....");
+			System.out.println("Connection: " +  connection_check);
+			System.out.println("Database Name: " + DBName + line + "Data: " + DBData);
 		
 		}catch(Exception e){  connection_check = false; System.out.println("Error:" + line +  "Connection: " + connection_check + line + e);}finally { GUI_Core.start_window(); /*If you fail to connect to a Database GUI_Core will continue to run*/}
 	
 }
 
+	public static void login_check(){
+	
+		//statement = connection.prepareStatement();
+	
+	}
+
 	public static void DBSearch(){
 	
 		try{
-		statement = connection.prepareStatement("");//what you want to search for
+		statement = connection.prepareStatement("SELECT * FROM `main`");//what you want to search for
 		DB_result = statement.executeQuery();
 	
 		//output = DB_result;
