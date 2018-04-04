44fe4eb0c752371dbb831531edf1c1e4bd3c8153
Christian Cuevas <ccpotential@gmail.com>
2018-04-04 12:02:47 -0700

    work
    Clark was here :V

--- discord_bot_js/commands/server.js
+++ discord_bot_js/commands/server.js
@@ -4,5 +4,32 @@ module.exports = {
   description: 'Profile on the current server.',
   args: false,
   guild_only: true,
-  execute(bot, msg, args) {},
+  execute(bot, msg, args) {
+    const guild = msg.guild;
+
+    msg.reply({
+      embed: {
+        color: 3447003,
+        author: {
+          name: bot.user.username,
+          icon_url: bot.user.avatarURL,
+        },
+        title: guild.name,
+        url: 'https://discord.js.org/#/',
+        thumbnail: {
+          url: guild.iconURL,
+        },
+        description: `Just another Discord Server with **${guild.memberCount}** members!`,
+        fields: [{
+          name: 'Server Information',
+          value: `Server Owner: *${guild.owner.user.tag}*\nServer Creation: *${guild.createdAt.toDateString()}*`,
+        }],
+        timestamp: new Date(),
+        footer: {
+          icon_url: bot.user.avatarURL,
+          text: bot.config.embed.footer,
+        },
+      },
+    });
+  },
 };
--- discord_bot_js/commands/users.js
+++ discord_bot_js/commands/users.js
@@ -1,8 +1,29 @@
+const Discord = require('Discord.js');
+
 module.exports = {
   name: 'users',
   aliases: ['us'],
-  description: 'Discord profile on users of the current server.',
+  description: 'Discord profile on current server\'s users.',
   args: false,
   guild_only: true,
-  execute(bot, msg, args) {},
+  execute(bot, msg, args) {
+    msg.reply('hello world!').then(reply => {
+      // Setup emojis
+      reply.react('⬅').then(() =>
+        reply.react('➡')).catch(() =>
+        console.log('Error: Sendind emojis'));
+
+      // const filter = (reaction, user) => {
+      //   console.log(reaction.emoji.name);
+      //   console.log(user.id);
+      //   console.log(msg.author.id);
+      //   return (reaction.emoji.name === '⬅' || reaction.emoji.name === '➡') && user.id === msg.author.id;
+      // };
+      // const collector = msg.awaitReactions(filter, {
+      //   time: 15000,
+      // });
+      // collector.on('collect', reaction => console.log(`Collected ${reaction.emoji.name}`));
+      // collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+    }).catch();
+  },
 };
