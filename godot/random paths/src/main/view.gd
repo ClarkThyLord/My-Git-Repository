d0888cb2c85f1f480a3615cc85cfd2bfbb7bbc7e
Christian Cuevas <ccpotential@gmail.com>
2018-08-24 11:30:26 -0700

    moving camera
    Clark was here :V

---
+++ godot/random paths/src/main/view.gd
@@ -0,0 +1,22 @@
+extends Camera2D
+
+export (int) var speed = 200
+var movement = Vector2()
+
+func _ready():
+	pass
+
+func _process(delta):
+	movement = Vector2()
+	if Input.is_action_pressed('right'):
+		movement.x += 1
+	if Input.is_action_pressed('left'):
+		movement.x -= 1
+	if Input.is_action_pressed('down'):
+		movement.y += 1
+	if Input.is_action_pressed('up'):
+		movement.y -= 1
+	
+	movement = (movement.normalized() * speed) * delta
+	
+	self.translate(movement)
