33a2e39fb5efa9fea034aaca4ce57122ddffdf6e
Christian Cuevas <ccpotential@gmail.com>
2018-08-30 15:08:39 -0700

    moved project
    Clark was here :V

--- godot/3d voxel editor/src/main/VoxelEditor.gd
+++ godot/3d voxel editor/src/main/VoxelEditor.gd
@@ -6,18 +6,12 @@ func _ready():
 	pass
 
 func _process(delta):
-	if Input.is_action_pressed("left_m"):
+	if Input.is_action_just_pressed("left_m"):
 		var from = get_viewport().get_camera().project_ray_origin(get_viewport().get_mouse_position())
 		var to = from + get_viewport().get_camera().project_ray_normal(get_viewport().get_mouse_position()) * raycast_length
 		
-		var space_state = get_world().get_direct_space_state()
-		var result = space_state.intersect_ray(from, to, [get_viewport().get_camera()])
-		
-		print(result)
+		var result = get_world().get_direct_space_state().intersect_ray(from, to, [get_viewport().get_camera()])
 		
 		if result.has('position'):
 			var position = world_to_map(result.position)
-			
-			print(position)
-			
 			set_cell_item(position.x, position.y, position.z, 0)
