bbf620a2b3a1e2698d3735824a38c63c6d6905eb
Christian Cuevas <ccpotential@gmail.com>
2018-09-25 15:13:22 -0700

    work? :v
    Clark was here :V

--- node.js/server/src/index.js
+++ node.js/server/src/index.js
@@ -1,7 +1,45 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+let DB = [];
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.send(`Hello world, I'm runing off port ${port}!`));
+
+app.get('/get', (req, res) => {
+	if (req.query.id) {
+		result = DB[req.query.id];
+	} else {
+		DB.filter((data) => {
+			for (let query of req.query) {
+				if (data[query]) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		});
+	}
+
+	res.send(JSON.stringify(result));
+});
+
+app.post('/add', (req, res) => {
+	let data = {
+		id: DB.length,
+		data: req.query
+	};
+
+	DB.push(data);
+
+	res.send(JSON.stringify({
+		success: true,
+		result: data
+	}));
+});
+
+app.delete("/remove/:id", (req, res) => {
+	delete DB[req.params.id];
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
