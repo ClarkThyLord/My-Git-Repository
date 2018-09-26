54d5e33aec29e5c67d357304a8d726325c24ee92
Christian Cuevas <ccpotential@gmail.com>
2018-09-25 22:25:58 -0700

    server for storing whatever on ram :v
    Clark was here :V

--- node.js/server/src/index.js
+++ node.js/server/src/index.js
@@ -1,18 +1,35 @@
 const express = require('express');
+const body_parser = require('body-parser');
+
 const app = express();
 const port = 3000;
 
-let DB = [];
+app.use(body_parser.json());
+
+let DB = [{
+		id: 0,
+		name: 'Christian'
+	},
+	{
+		id: 1,
+		name: 'ClarkThyLord'
+	}
+];
 
 app.get('/', (req, res) => res.send(`Hello world, I'm runing off port ${port}!`));
 
 app.get('/get', (req, res) => {
-	if (req.query.id) {
-		result = DB[req.query.id];
-	} else {
-		DB.filter((data) => {
-			for (let query of req.query) {
-				if (data[query]) {
+	let response = {
+		success: false,
+		data: []
+	};
+
+	if (req.query.id && DB[req.query.id]) {
+		response.data.push(DB[req.query.id]);
+	} else if (Object.keys(req.query).length > 0) {
+		response.data = DB.filter((data) => {
+			for (let [query_k, query_v] of Object.entries(req.query)) {
+				if (data[query_k] == query_v) {
 					return true;
 				} else {
 					return false;
@@ -21,25 +38,62 @@ app.get('/get', (req, res) => {
 		});
 	}
 
-	res.send(JSON.stringify(result));
+	if (response.data.length > 0) {
+		response.success = true;
+	}
+
+	res.send(JSON.stringify(response));
+});
+
+app.get('/getAll', (req, res) => {
+	res.send(JSON.stringify(DB));
 });
 
 app.post('/add', (req, res) => {
-	let data = {
-		id: DB.length,
-		data: req.query
+	let response = {
+		success: true,
+		data: undefined
 	};
 
-	DB.push(data);
+	DB.push(Object.assign({
+		id: DB.length
+	}, req.body));
 
-	res.send(JSON.stringify({
-		success: true,
-		result: data
-	}));
+	response.data = DB[DB.length - 1];
+
+	res.send(JSON.stringify(response));
 });
 
 app.delete("/remove/:id", (req, res) => {
-	delete DB[req.params.id];
+	let response = {
+		success: false,
+		data: {}
+	};
+
+	if (DB[req.params.id]) {
+		response.success = true;
+		response.data = DB[req.params.id];
+
+		delete DB[req.params.id];
+	}
+
+	res.send(JSON.stringify(response));
+});
+
+app.post("/update/:id", (req, res) => {
+	let response = {
+		success: false,
+		data: {}
+	};
+
+	if (DB[req.params.id]) {
+		response.success = true;
+		if (req.body["id"]) delete req.body["id"];
+		DB[req.params.id] = Object.assign(DB[req.params.id], req.body);
+		response.data = DB[req.params.id];
+	}
+
+	res.send(JSON.stringify(response));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
