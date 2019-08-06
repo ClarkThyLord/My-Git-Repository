db181c970dfbab417081488c5dc314b26c05f7cc
Christian Cuevas <ccpotential@gmail.com>
2019-08-05 23:13:55 -0700

    built some logic for VirtualJoyStick of Rogue
    Clark was here ;V

--- godot/Rogue/src/VirtualJoystick/VirtualJoystick.gd
+++ godot/Rogue/src/VirtualJoystick/VirtualJoystick.gd
@@ -10,6 +10,18 @@ onready var Top : TextureRect = $Base/Top
 
 
 # Declarations
+signal activated(active)
+var Active : bool = false setget set_active
+func set_active(active : bool) -> void:
+	Active = active
+	Top.set_anchors_and_margins_preset(Control.PRESET_CENTER, Control.PRESET_MODE_KEEP_SIZE, 0)
+	Base.visible = active
+	emit_signal('activated', active)
+
+
+export(bool) var Boundless : bool = true
+
+
 export(int) var BaseSize : int = 100 setget set_base_size
 func set_base_size(size : int) -> void:
 	BaseSize = size
@@ -42,6 +54,23 @@ func _ready():
 	set_top_texture(TopTexture)
 
 
+func _input(event):
+	if Active and event is InputEventMouseMotion:
+		event = Base.make_input_local(event)
+		Top.set_position(event.position - Top.get_size() / 2)
+
+
 func _on_VirtualJoystick_input(event):
 	if event is InputEventMouseButton and event.pressed:
+		set_active(true)
+		event = make_input_local(event)
 		Base.set_position(event.position - Base.get_size() / 2)
+
+
+func _on_Base_input(event): pass
+#	if Active and event is InputEventMouseMotion:
+#		event = make_input_local(event)
+#		Top.set_position(event.position - Top.get_size() / 2)
+
+func _on_Base_mouse_exited():
+	if not Boundless: set_active(false)
