f430d9c6ed42711f26b16868140dbb4e21a79772
ClarkThyLord <ccpotential@gmail.com>
2019-09-05 16:24:19 -0700

    fixed up

---
+++ godot/Slime Kingdoms/src/Player/Player.gd
@@ -0,0 +1,42 @@
+extends Camera2D
+class_name Player
+
+
+
+# Declarations
+export(String) var Name : String = ''
+
+export(int) var Speed : int = 200
+export(int) var SpeedBoost : int = 2
+
+var selected : Array = []
+func set_selected(nodes : Array) -> void: selected = nodes
+func add_selected(node : Node) -> void: if not selected.has(node): selected.append(node)
+func remove_selected(node : Node) -> void: selected.erase(node)
+
+
+
+# Core
+func _process(delta):
+	if current and selected.size() == 0:
+		var movement = Vector2()
+		if Input.is_action_pressed('move_up'): movement.y = -1
+		if Input.is_action_pressed('move_right'): movement.x = 1
+		if Input.is_action_pressed('move_down'): movement.y = 1
+		if Input.is_action_pressed('move_left'): movement.x = -1
+		
+		if Input.is_action_pressed('move_boost') and not Input.is_action_pressed('attack'):
+			movement *= SpeedBoost
+		
+		translate((movement * Speed) * delta)
+	elif current and selected.size() > 1:
+		var x1 = selected[0].position.x
+		var x2 = selected[0].position.x
+		var y1 = selected[0].position.y
+		var y2 = selected[0].position.y
+		for node in selected:
+			if node.position.x < x1: x1 = node.position.x
+			if node.position.x > x1: x2 = node.position.x
+			if node.position.y < y1: y1 = node.position.y
+			if node.position.y > y2: y2 = node.position.y
+		set_position(Vector2(x2 - x1, y2 - y1))
---
+++ godot/Slime Kingdoms/src/Slime/Slime.gd
@@ -0,0 +1,83 @@
+extends KinematicBody2D
+
+
+
+# Declarations
+export(bool) var Selected : bool = false setget set_selected
+func set_selected(selected : bool) -> void:
+	Selected = selected
+	if Selected:
+		$Sprite.scale = Vector2(1.30, 1.30)
+#		This doesn't return the current Camera2D >_>
+#		if get_viewport().get_camera(): get_viewport().get_camera().add_selected(self)
+	else:
+		$Sprite.scale = Vector2(1, 1)
+#		This doesn't return the current Camera2D >_>
+#		if get_viewport().get_camera(): get_viewport().get_camera().remove_selected(self)
+
+export(String) var Name = ''
+export(int) var Health : int = 25 
+
+export(int) var Defense : int = 3
+
+export(int) var Speed : int = 15
+export(float) var SpeedBoost : float = 2.0
+
+export(int) var Attack : int = 3
+enum AttackMode { SingleAttack, AreaAttack }
+export(AttackMode) var SlimeAttackMode = AttackMode.SingleAttack
+
+export(Color) var SlimeColor : Color = Color(1, 1, 1) setget set_slime_color
+func set_slime_color(color : Color):
+	modulate = color
+	SlimeColor = color
+
+
+
+# Core
+func _process(delta):
+	if Selected:
+		var movement = Vector2()
+		if Input.is_action_pressed('move_up'): movement.y = -1
+		if Input.is_action_pressed('move_right'): movement.x = 1
+		if Input.is_action_pressed('move_down'): movement.y = 1
+		if Input.is_action_pressed('move_left'): movement.x = -1
+		
+		if movement.x < 0:
+			$AttackRange.cast_to.x = -17
+			$Sprite.flip_h = false
+		elif movement.x > 0:
+			$Sprite.flip_h = true
+			$AttackRange.cast_to.x = 17
+		
+		if Input.is_action_pressed('move_boost') and not Input.is_action_pressed('attack'):
+			movement *= SpeedBoost
+		
+		if Input.is_action_pressed('attack'):
+			if not $AnimationPlayer.current_animation == 'attacking': $AnimationPlayer.play('attacking')
+		elif Input.is_action_pressed('move_boost'): $AnimationPlayer.play('jumping')
+		elif movement.length() != 0: $AnimationPlayer.play('moving')
+		else: $AnimationPlayer.play('idle')
+		
+		translate((movement * Speed) * delta)
+
+func attack() -> void:
+#	print('attacked')
+	var attack_damage = randi() % (Attack + 1)
+	if SlimeAttackMode == AttackMode.SingleAttack:
+		print('single attack : ' + str(attack_damage))
+	elif SlimeAttackMode == AttackMode.AreaAttack:
+		print('area attack : ' + str(attack_damage))
+
+func _on_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		print('selected')
+		set_selected(!Selected)
+
+func _on_mouse_entered():
+#	print('hover')
+	$Sprite.scale = Vector2(1.30, 1.30)
+
+func _on_mouse_exited():
+#	print('unhover')
+	if not Selected: $Sprite.scale = Vector2(1, 1)
