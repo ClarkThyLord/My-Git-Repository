5f84e9c3adcddac044a75b29adef3f8546e5ffe5
Christian Cuevas <ccpotential@gmail.com>
2019-09-11 19:56:36 -0700

    food pool
    Clark was here :V

--- godot/Slime Kingdoms/src/Food/Food.gd
+++ godot/Slime Kingdoms/src/Food/Food.gd
@@ -1,15 +1,24 @@
 tool
 extends Node2D
 
+
+
+# Declarations
+signal eaten(_self)
+
 export(int) var Wealth : int = 1
 export(Color) var FoodColor : Color = Color(1, 1, 1) setget set_food_color
 func set_food_color(color : Color) -> void:
 	FoodColor = color
 	update()
 
+
+
+# Core
 func _draw(): draw_circle(Vector2(), 2, FoodColor)
 
 func _on_area_entered(area):
 	if area.get_parent().is_in_group('slimes'):
 		area.get_parent().eat(Wealth, FoodColor)
-	queue_free()
+		emit_signal('eaten', self)
+#		queue_free()
--- godot/Slime Kingdoms/src/World/World.gd
+++ godot/Slime Kingdoms/src/World/World.gd
@@ -8,14 +8,41 @@ const Food := preload('res://src/Food/Food.tscn')
 
 
 # Declarations
-var food_count : int = 0
+var foods : Array = [] setget set_foods
+func set_foods(foods : Array) -> void: pass
+var foods_queue : Array = [] setget set_foods
+func set_foods_queue(foods : Array) -> void: pass
 export(int) var FoodWealth : int = 10
 export(int) var FoodMaximum : int = 100
 
+
+
+# Core
 func _process(delta):
-	if food_count < FoodMaximum:
+	if foods.size() < FoodMaximum:
 		var food := Food.instance()
+		
 		food.position = Vector2(randi() % 400, randi() % 400)
 		food.Wealth = (randi() % FoodWealth) + 1
 		food.FoodColor = Color(randf(), randf(), randf())
+		
+		food.connect('eaten', self, 'queue_food')
+		
+		foods.append(food)
 		add_child(food)
+	elif foods_queue.size() > 0:
+		var food = foods[0]
+		
+		food.position = Vector2(randi() % 400, randi() % 400)
+		food.Wealth = (randi() % FoodWealth) + 1
+		food.FoodColor = Color(randf(), randf(), randf())
+		
+		foods.append(food)
+		add_child(food)
+
+func queue_food(food) -> void:
+	if foods.has(food) and not foods_queue.has(food):
+		foods.erase(food)
+		call_deferred('remove_child', food)
+#		remove_child(food)
+		queue_food(food)
