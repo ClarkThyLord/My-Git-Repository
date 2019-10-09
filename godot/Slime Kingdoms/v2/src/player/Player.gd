fd83587f8c4a33a7c473b6c6c37016a1de1035c7
Christian Cuevas <ccpotential@gmail.com>
2019-10-08 21:25:52 -0700

    player zoom
    Clark was here :V

--- godot/Slime Kingdoms/v2/src/player/Player.gd
+++ godot/Slime Kingdoms/v2/src/player/Player.gd
@@ -25,13 +25,18 @@ func _process(delta) -> void:
 		translate(movement * Speed * delta)
 
 func _unhandled_input(event : InputEvent) -> void:
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and not event.is_pressed():
-		var targets = get_world_2d().direct_space_state.intersect_point(get_global_mouse_position())
-		for target in targets:
-			if target['collider'].is_in_group('entities'):
-				if target['collider'].Selector == self:
-					target['collider'].unselected()
-					selected.append(target['collider'])
-				else:
-					target['collider'].selected(self)
-					selected.append(target['collider'])
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and not event.is_pressed():
+			var targets = get_world_2d().direct_space_state.intersect_point(get_global_mouse_position())
+			for target in targets:
+				if target['collider'].is_in_group('entities'):
+					if target['collider'].Selector == self:
+						target['collider'].unselected()
+						selected.append(target['collider'])
+					else:
+						target['collider'].selected(self)
+						selected.append(target['collider'])
+		elif event.button_index == BUTTON_WHEEL_UP and zoom.x > 0.1 and zoom.y > 0.1:
+			zoom -= Vector2(0.1, 0.1)
+		elif event.button_index == BUTTON_WHEEL_DOWN:
+			zoom += Vector2(0.1, 0.1)
